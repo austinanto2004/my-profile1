8667f9d956c7ec798fe14de33fa44d67da45358a
austinanto2004 <142858153+austinanto2004@users.noreply.github.com>
2025-02-01 20:48:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => console.log("MongoDB Connected"))
-.catch(err => console.error(err));
+  .catch(err => console.error(err));
