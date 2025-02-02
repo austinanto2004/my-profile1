11164a4645891eaccd9adf04d13b2bab7ea129dd
austinanto2004 <142858153+austinanto2004@users.noreply.github.com>
2025-02-02 18:50:18 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
 
-mongoose.connect(MONGO_URI);
+mongoose.connect(mongodb+srv://austinantoignatius:<db_password>@austi.pnwo1.mongodb.net/?retryWrites=true&w=majority&appName=austi
+);
 console.log("MongoDB Connected")
 
